20637685372332ca56df371a39e7cd32366e1ad3
Miloš Janković <mjankovic180@gmail.com>
2020-01-25 16:31:07 +0100

    init

---
+++ js/tvitovi.js
@@ -0,0 +1,50 @@
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer ");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+var tvit = $('.tvit').prop('outerHTML');
+$("#tvitovi > #template-id").remove();
+
+/*https://cors-anywhere.herokuapp.com/*/
+
+function emputy() {
+  let newHTML = tvit.replace('hide', '');
+  newHTML = newHTML.replace('neki-tekst-tvita', "Pokusajte sa unosom drugog pojma");
+  newHTML = newHTML.replace('template-username', "Nema tvitova za zadati pojam");
+  newHTML = newHTML.replace('template-element', "nema_tvitova");
+  $('#tvitovi').append(newHTML);
+}
+
+function populate(res) {
+  let statusi = JSON.parse(res);
+  console.log(statusi);
+  if (statusi["statuses"].length < 1) {
+    emputy();
+  } else {
+    statusi["statuses"].forEach(element => {
+        let newHTML = tvit.replace('hide', '');
+        newHTML = newHTML.replace('template-text', element.text);
+        newHTML = newHTML.replace('template-id', element.id);
+        newHTML = newHTML.replace('template-username', element.user.screen_name);
+        newHTML = newHTML.replace('template-username', element.user.screen_name);
+        $('#tvitovi').append(newHTML);
+    });
+  }
+}
+
+document.getElementById("search").addEventListener('submit', (e) => {
+  e.preventDefault();
+  document.getElementById('tvitovi').innerHTML = "";
+  let word = document.getElementById("word").value
+  console.log(word)
+  if (word == "") emputy();
+  else fetch("https://api.twitter.com/1.1/search/tweets.json?q=" + word + "&count=100", requestOptions)
+    .then(response => response.text())
+    .then(result => populate(result))
+    .catch(error => console.log('error', error));
+})
